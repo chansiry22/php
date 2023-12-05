f7e5426570a72820ca4c619ac9f11a5172146dc8
shishansiry22 <Bong.Rey10122001@gmail.com>
2023-12-05 19:15:22 +0700

    edit title dashboard

--- STOCK/Public/HTML/Admin-Dashboard/Admin-Dahboard.php
+++ STOCK/Public/HTML/Admin-Dashboard/Admin-Dahboard.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Public/CSS/Admin-Dashboard.css">
-    <title>Dashboard Admin</title>
+    <title>Dashboard</title>
 </head>
 <body>
     <nav class="sidebar close">
