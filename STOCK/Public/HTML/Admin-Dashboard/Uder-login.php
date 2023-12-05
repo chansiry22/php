2a56aef4f851f3e3a2185ed729288d505c70e5b0
samplerigthgithub <detsarat111@gmail.com>
2023-12-05 23:46:49 +0700

    add login file

---
+++ STOCK/Public/HTML/Admin-Dashboard/Uder-login.php
@@ -0,0 +1,19 @@
+<?php 
+    include("../Link-Header/Link.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container-fluird">
+        <form action="" method="post">
+            <input type="text" class="form-controll">
+            <button type="submit"></button>
+        </form>
+    </div>
+</body>
+</html>
